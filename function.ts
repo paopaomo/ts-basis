24dfa7aa9e17e747cbda9c076a236f8c018abe74
Zi Ye <18612319597@163.com>
2020-07-01 21:33:10 +0800

    function this

--- function.ts
+++ function.ts
@@ -15,3 +15,54 @@ const buildName = (firstName = 'Will', lastName?: string): string => {
 const result1 = buildName('Zi', 'Ye');
 const result2 = buildName(undefined, 'Adams');
 const result3 = buildName();
+
+// this
+interface Card {
+    suit: string;
+    card: number
+}
+
+interface Deck {
+    suits: string[];
+    cards: number[];
+    createCardPicker(this: Deck): () => Card
+}
+
+const deck: Deck = {
+    suits: ['hearts', 'spades', 'clubs', 'diamonds'],
+    cards: new Array(52),
+    createCardPicker: function(this: Deck) {
+        return () => {
+            const pickedCard = Math.floor(Math.random() * 52);
+            const pickedSuit = Math.floor(pickedCard / 13);
+            return {
+                suit: this.suits[pickedSuit],
+                card: pickedCard % 13
+            }
+        }
+    }
+};
+
+const cardPicker = deck.createCardPicker();
+const pickedCard = cardPicker();
+
+console.log(`Card: ${pickedCard.card} of ${pickedCard.suit}`);
+
+interface UIElement {
+    addClickListener(onclick: (this: void, e: Event) => void): void;
+}
+
+class Handler {
+    type: string;
+
+    onClickBad = (e: Event) => {
+        this.type = e.type;
+    }
+}
+
+const handler = new Handler();
+const uiElement: UIElement = {
+    addClickListener() {}
+};
+
+uiElement.addClickListener(handler.onClickBad);
